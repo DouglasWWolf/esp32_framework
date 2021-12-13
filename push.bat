fd27df158f725c15f21aa596e540691ee25bdba3
DouglasWWolf <douglaswolf@yahoo.com>
2021-12-12 22:34:37 -0800

    See History.h for changes

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
 rem rd/s/q build
 git add main/*.cpp main/*.h main/component.mk main/CMakeLists.txt
-git add CMakeLists.txt partitions.csv sdkconfig
+git add CMakeLists.txt *.csv sdkconfig
 git add push.bat
 git commit -m "See History.h for changes"
 git push origin main
